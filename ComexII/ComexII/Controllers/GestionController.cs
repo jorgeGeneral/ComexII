ede4ad7b20c9fa8270cf1f5280c05d337ec85fd8
efuentes <fc.kike@gmail.com>
2020-04-28 14:07:18 -0400

    Cambio en estructura de carpetas del proyecto se genera nuevos controller y carpetas de vistas

---
+++ ComexII/ComexII/Controllers/GestionController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComexII.Controllers
+{
+    public class GestionController : Controller
+    {
+        // GET: Gestion
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ComexIIConsultaClienteTarifado()
+        {
+            return View();
+        }
+        
+        public ActionResult ComexIIConsultaPrecios()
+        {
+            return View();
+        }
+        public ActionResult ComexIIConsultaHistoricoCampañas()
+        {
+            return View();
+        }
+        public ActionResult ComexIITarifasCampañasHistorica()
+        {
+            return View();
+        }
+        public ActionResult ComexIIConsultaHistoricoPreciosEspeciales()
+        {
+            return View();
+        }
+        public ActionResult ComexIIConsultaCampañas()
+        {
+            return View();
+        }
+        public ActionResult ComexIINuevaCampaña()
+        {
+            return View();
+        }
+        public ActionResult ComexIIEstructuraPrecio()
+        {
+            return View();
+        }
+        public ActionResult ComexIITarifasCampañas()
+        {
+            return View();
+        }
+        public ActionResult ComexIIBandejaAutorizaciones()
+        {
+            return View();
+        }
+        public ActionResult ComexIIMonitoreoCompraDivisa()
+        {
+            return View();
+        }
+        public ActionResult ComexIIHistoricoCompraDivisa()
+        {
+            return View();
+        }
+    }
+}
--- ComexII/ComexII/Controllers/HomeController.cs
+++ ComexII/ComexII/Controllers/HomeController.cs
@@ -12,109 +12,11 @@ namespace ComexII.Controllers
         {
             return View();
         }
-
-        public ActionResult ComexIIEstructuraPrecio()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIIMatrizTarifado()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIIParametrizacionGastos()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIIParametros()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIIPreciosEspeciales()
-        {
-            return View();
-        }
-   
-        public ActionResult ComexIITarifasCampañas()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIIRutCliente()
-        {
-            return View();
-        }
-
         public ActionResult ComexIIConsultaDetalleCliente()
         {
             return View();
         }
-
         public ActionResult ComexIIClonaDatos()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIIConsultaClienteTarifado()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIITramosPorSegmento()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIIBandejaAutorizaciones()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIIConsultaCampañas()
-        {
-            return View();
-        }
-
-        public ActionResult ComexIIMonitoreoCompraDivisa()
-        {
-            return View();
-        }
-        public ActionResult ComexIIHistoricoCompraDivisa()
-        {
-            return View();
-        }
-        public ActionResult ComexIIParametroControlCompraDivisa()
-        {
-            return View();
-        }
-        public ActionResult ComexIIControlTiempoCompraDivisa()
-        {
-            return View();
-        }
-        public ActionResult ComexIIRutClientePreciosEspeciales()
-        {
-            return View();
-        }
-        public ActionResult ComexIINuevaCampaña()
-        {
-            return View();
-        }
-        public ActionResult ComexIIConsultaPrecios()
-        {
-            return View();
-        } 
-        public ActionResult ComexIIConsultaHistoricoCampañas()
-        {
-            return View();
-        }
-        public ActionResult ComexIITarifasCampañasHistorica()
-        {
-            return View();
-        }
-        public ActionResult ComexIIConsultaHistoricoPreciosEspeciales()
         {
             return View();
         }
