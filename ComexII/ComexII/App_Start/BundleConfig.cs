a8684898aa65dca09c810913e1227fefd8d03acc
jorge rivera <jorge.rivera.hernandez@gmail.com>
2020-06-24 17:39:44 -0400

    Nueva funcion para cargar grilla de pantalla estructura de precios por segmento, se creo una clase nueva cargagrilla

--- ComexII/ComexII/App_Start/BundleConfig.cs
+++ ComexII/ComexII/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace ComexII
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js", "~/Scripts/general.js", "~/Scripts/cargaCombo.js"));
+                      "~/Scripts/bootstrap.js", "~/Scripts/general.js", "~/Scripts/CargaCombo.js", "~/Scripts/CargaGrilla.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
